58d738ccc312d6c3a6105e92424bda77db294798
VikasVerma-15 <97469119+VikasVerma-15@users.noreply.github.com>
2025-12-30 11:45:57 +0530

    Update Tour.js

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,34 +1,27 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
-const Tour = ({ id, image, info, name, price, removeTour }) => {
+function Tour({ id, name, info, image, price }) {
   const [readMore, setReadMore] = useState(false);
 
   return (
-    <article className="single-tour">
+    <div>
+      <h2>{name}</h2>
       <img src={image} alt={name} />
 
-      <footer>
-        <div className="tour-info">
-          <h4>{name}</h4>
-          <h4 className="tour-price">${price}</h4>
-        </div>
-
-        <p>
-          {readMore ? info : `${info.substring(0, 200)}...`}
-          <button
-            className="info-btn"
-            onClick={() => setReadMore(!readMore)}
-          >
-            {readMore ? "See less" : "Show more"}
+      <p id={`tour-item-para-${id}`}>
+        {readMore ? info : info.split(" ").slice(0, 200).join(" ")}
+        {info.split(" ").length > 200 && (
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? " Show Less" : " Read More"}
           </button>
-        </p>
+        )}
+      </p>
 
-        <button className="delete-btn" onClick={() => removeTour(id)}>
-          Remove
-        </button>
-      </footer>
-    </article>
+      <button id={`delete-btn-${id}`}>
+        Not Interested
+      </button>
+    </div>
   );
-};
+}
 
 export default Tour;
