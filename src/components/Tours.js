6cb53c4940257aa3c0b57a5190b096ffefab42b3
VikasVerma-15 <97469119+VikasVerma-15@users.noreply.github.com>
2026-01-03 16:56:19 +0530

    Update Tours.js

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -3,18 +3,18 @@ import Tour from "./Tour";
 
 const Tours = ({ tours, removeTour }) => {
   return (
-    <section>
-      <div className="title">
-        <h2>Our Tours</h2>
-        <div className="underline"></div>
-      </div>
-
+    <>
       {tours.map((tour) => (
-        <Tour key={tour.id} {...tour} removeTour={removeTour} />
+        <Tour
+          key={tour.id}
+          {...tour}
+          removeTour={removeTour}
+        />
       ))}
-    </section>
+    </>
   );
 };
 
 export default Tours;
+
 
