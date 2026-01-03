ab5fb762a34da272f123969df77a4a17dae9547c
VikasVerma-15 <97469119+VikasVerma-15@users.noreply.github.com>
2026-01-03 16:56:35 +0530

    Update Tour.js

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -4,37 +4,32 @@ function Tour({ id, name, info, image, price, removeTour }) {
   const [readMore, setReadMore] = useState(false);
 
   const words = info.split(" ");
-  const first200Words = words.slice(0, 200).join(" ");
-  const hasMoreThan200Words = words.length > 200;
+  const shortText = words.slice(0, 200).join(" ");
 
   return (
-    <div>
+    <article>
       <h2>{name}</h2>
       <img src={image} alt={name} />
       <h4>{price}</h4>
 
-      {/* REQUIRED by Cypress */}
+      {/* Cypress looks for THIS EXACT ID */}
       <p id={`tour-item-para-${id}`}>
-        {readMore || !hasMoreThan200Words ? info : first200Words}
-
-        {hasMoreThan200Words && (
-          <button onClick={() => setReadMore(!readMore)}>
-            {readMore ? " Show Less" : " Read More"}
-          </button>
-        )}
+        {readMore ? info : shortText}
+        <button onClick={() => setReadMore(!readMore)}>
+          {readMore ? " Show Less" : " Read More"}
+        </button>
       </p>
 
-      {/* REQUIRED by Cypress */}
+      {/* Cypress looks for THIS EXACT ID */}
       <button
         id={`delete-btn-${id}`}
         onClick={() => removeTour(id)}
       >
         Not Interested
       </button>
-    </div>
+    </article>
   );
 }
 
 export default Tour;
-
 
