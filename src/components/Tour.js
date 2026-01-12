702cdf5a2f2188cb760089c9c628a6bd3a4b45ce
Vikas Verma <vikasverma15@gmail.com>
2026-01-12 23:38:25 +0530

    change

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -3,23 +3,22 @@ import React, { useState } from "react";
 function Tour({ id, name, info, image, price, removeTour }) {
   const [showMore, setShowMore] = useState(false);
 
-  const shortText = info.split(" ").slice(0, 200).join(" ");
+  const shortText = info.slice(0, 250);
 
   return (
     <div>
       <h2>{name}</h2>
       <img src={image} alt={name} />
       <h4>{price}</h4>
-
-      <p id={`tour-item-para-${id}`}>
-        {showMore ? info : shortText}
-        <button
-          id={showMore ? `see-less-${id}` : `see-more-${id}`}
-          onClick={() => setShowMore(!showMore)}
-        >
-          {showMore ? "See less" : "See more"}
-        </button>
-      </p>
+<p id={`tour-item-para-${id}`}>
+  {showMore ? info : shortText}
+  <button
+    id={showMore ? `see-less-${id}` : `see-more-${id}`}
+    onClick={() => setShowMore(!showMore)}
+  >
+    {showMore ? "See less" : "See more"}
+  </button>
+</p>
 
       <button id={`delete-btn-${id}`} onClick={() => removeTour(id)}>
         Not Interested
