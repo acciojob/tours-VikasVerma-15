6bbdc69ea66e12d7d03e6686d9d0129a9276ad4c
Vikas Verma <vikasverma15@gmail.com>
2026-01-13 20:22:54 +0530

    add

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 function Tour({ id, name, info, image, price, removeTour }) {
   const [showMore, setShowMore] = useState(false);
 
-  const shouldShowButton = info.length > 200;
+  const shouldTruncate = info.length > 200;
   let shortText = info.length > 200 ? info.slice(0, 200) : info;
 
   const lastSpaceIndex = shortText.lastIndexOf(" ");
@@ -17,14 +17,12 @@ function Tour({ id, name, info, image, price, removeTour }) {
       <p id={`tour-item-para-${id}`} className="tour-info">
         {showMore ? info : shortText}
       </p>
-      {shouldShowButton && (
-        <button
-          id={showMore ? `see-less-${id}` : `see-more-${id}`}
-          onClick={() => setShowMore(!showMore)}
-        >
-          {showMore ? "See less" : "See more"}
-        </button>
-      )}
+      <button
+        id={showMore ? `see-less-${id}` : `see-more-${id}`}
+        onClick={() => shouldTruncate && setShowMore(!showMore)}
+      >
+        {showMore ? "See less" : "See more"}
+      </button>
 
       <button className="delete-btn" id={`delete-btn-${id}`} onClick={() => removeTour(id)}>
         Not Interested
